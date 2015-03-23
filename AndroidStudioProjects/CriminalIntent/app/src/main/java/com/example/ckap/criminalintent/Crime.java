32d8cac43b371ee80d0e305616f05e8252d06fa9
Chandresh Kapadia <ckap@Chandreshs-MacBook-Pro.local>
2015-03-22 17:16:33 -0700

    Chapter 9: Understanding how layout parameters work.

--- AndroidStudioProjects/CriminalIntent/app/src/main/java/com/example/ckap/criminalintent/Crime.java
+++ AndroidStudioProjects/CriminalIntent/app/src/main/java/com/example/ckap/criminalintent/Crime.java
@@ -1,5 +1,6 @@
 package com.example.ckap.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -8,6 +9,30 @@ import java.util.UUID;
 public class Crime {
     private UUID mId;
     private String mTitle;
+
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime() {
+        // Generate unique identifier.
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date mDate) {
+        this.mDate = mDate;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean mSolved) {
+        this.mSolved = mSolved;
+    }
 
     public String getTitle() {
         return mTitle;
@@ -19,10 +44,5 @@ public class Crime {
 
     public UUID getId() {
         return mId;
-    }
-
-    public Crime() {
-        // Generate unique identifier.
-        mId = UUID.randomUUID();
     }
 }
